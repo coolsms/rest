cd8d1499b7364c0f7fc8643d8fe245e75a03728e
wiley <wiley@nurigo.net>
2013-12-20 11:33:15 +0900

    python rest api added

---
+++ python/coolsms.py
@@ -0,0 +1,148 @@
+# -*- coding: utf8 -*-
+"""
+ Copyright (C) 2008-2013 NURIGO
+ http://www.coolsms.co.kr
+"""
+
+__version__ = "2.3"
+
+from hashlib import md5
+import httplib,urllib,sys,hmac
+
+class rest:
+	api_key = None
+	api_secret = None
+
+	def __init__(self):
+		self.api_key = None
+		self.api_secret = None
+
+	def __init__(self, api_key, api_secret):
+		self.api_key = api_key
+		self.api_secret = api_secret
+
+	def md5(self, str):
+		if sys.version_info[1] < 6:
+			#- ptyon version 2.6.X under
+			hash = md5.new()
+		else:
+			#- ptyon version 2.6.X upper
+			hash = hashlib.md5()
+		hash.update(str)
+		return string.join(map(lambda v: "%02x" % ord(v), hash.digest()), "")
+
+	def send(self,to,sender,text):
+		#to = "01048597580,01048597580"
+		#to = "0100000000"
+		#sender = "01048597580"
+		#text = "Hello CoolSMS"
+		salt = "1234"
+		signature = hmac.new(self.api_secret, salt, md5)
+
+		conn = httplib.HTTPConnection("apitest.coolsms.co.kr", 2000)
+		params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'to':to, 'from':sender, 'text':text, 'datetime':'20131212160000'})
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn.request("POST", "/1/send", params, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		print data
+		conn.close()
+
+	def status(self, mid = None, gid = None):
+		if mid == None and gid == None:
+			return
+		salt = "1234"
+		signature = hmac.new(self.api_secret, salt, md5)
+
+		conn = httplib.HTTPConnection("apitest.coolsms.co.kr", 2000)
+		if mid:
+			params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'mid':mid})
+		if gid:
+			params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'gid':gid})
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn.request("GET", "/1/sent?" + params, None, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		print data
+		conn.close()
+
+	def balance(self):
+		salt = "1234"
+		signature = hmac.new(self.api_secret, salt, md5)
+
+		conn = httplib.HTTPConnection("apitest.coolsms.co.kr", 2000)
+		params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest()})
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn.request("GET", "/1/balance?" + params, None, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		print data
+		conn.close()
+
+	def set_report_url(self):
+		salt = "1234"
+		signature = hmac.new(self.api_secret, salt, md5)
+
+		conn = httplib.HTTPConnection("apitest.coolsms.co.kr", 2000)
+		params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'url':'http://nurigo.net'})
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn.request("POST", "/1/report_url", params, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		print data
+		conn.close()
+
+	def get_report_url(self):
+		salt = "1234"
+		signature = hmac.new(self.api_secret, salt, md5)
+
+		conn = httplib.HTTPConnection("apitest.coolsms.co.kr", 2000)
+		params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest()})
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn.request("GET", "/1/report_url?" + params, None, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		print data
+		conn.close()
+
+	def cancel(self, mid = None, gid = None):
+		if mid == None and gid == None:
+				return
+
+		salt = "1234"
+		signature = hmac.new(self.api_secret, salt, md5)
+
+		conn = httplib.HTTPConnection("apitest.coolsms.co.kr", 2000)
+		if mid:
+			params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'mid':mid})
+		if gid:
+			params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'gid':gid})
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn.request("POST", "/1/cancel", params, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		print data
+		conn.close()
+def main():
+	api_key = "NCS52A57F48C3D32"
+	api_secret = "5AC44E03CE8E7212D9D1AD9091FA9966"
+	mid = "M52A95079DE2B0";
+	gid = "G52A95079DDA04";
+
+	r = rest(api_key, api_secret)
+	#r.send()
+	r.status(gid=gid)
+	#r.balance()
+	#r.set_report_url()
+	#r.cancel(gid=gid)
+	#r.get_report_url()
+
+if __name__ == "__main__":
+	main()
+	sys.exit(0)
---
+++ python/example.py
@@ -0,0 +1,54 @@
+import gui
+import coolsms
+
+def load(evt):
+	return
+
+def send_message(evt):
+	api_key = "NCS52A57F48C3D32"
+	api_secret = "5AC44E03CE8E7212D9D1AD9091FA9966"
+
+	to = mywin['to'].value
+	sender = mywin['from'].value
+	message = mywin['message'].value.encode('utf-8')
+	#print message
+	#message = "TEST"
+	r = coolsms.rest(api_key, api_secret)
+	r.send(to,sender,message)
+	#r.status(gid=gid)
+	#r.balance()
+	#r.set_report_url()
+	#r.cancel(gid=gid)
+	#r.get_report_url()
+	gui.alert("Sent!")
+
+
+def close_window(evt):
+	exit()
+
+gui.Window(name='mywin', title='COOLSMS Messaging'
+		, resizable=True, height='610px'
+		, left='180', top='24'
+		, width='396px', bgcolor='#E0E0E0')
+
+gui.TextBox(name='message', left='0', top='0', width='150', height='160', parent='mywin', value='message\nhere', multiline=True)
+
+gui.TextBox(name='to', left='0', top='170', width='150', parent='mywin', value='01000000000')
+gui.TextBox(name='from', left='0', top='190', width='150', parent='mywin', value='01000000000')
+
+gui.Button(label='Send', name='send', left='196', top='50', width='85', default=True, parent='mywin')
+gui.Button(label='Quit', name='close', left='196', top='80', width='85', default=True, parent='mywin')
+gui.StatusBar(name='statusbar', parent='mywin')
+
+mywin = gui.get("mywin")
+
+#mywin.onload = load
+mywin['send'].onclick = send_message
+mywin['close'].onclick = close_window
+#mywin['slider'].onclick = slider_click
+
+if __name__ == "__main__":
+	mywin.show()
+	mywin.title = "COOLSMS"
+	mywin['statusbar'].text = "COOLSMS Status Bar"
+	gui.main_loop()
