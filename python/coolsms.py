abce39d52f1df5a456bd7032b1f5fe9be3e08d10
wiley <wiley@nurigo.net>
2013-12-27 11:43:08 +0900

    샘플 어플리케이션 Setup, Message, List 탭으로 구성

--- python/coolsms.py
+++ python/coolsms.py
@@ -7,7 +7,43 @@
 __version__ = "2.3"
 
 from hashlib import md5
-import httplib,urllib,sys,hmac
+import httplib,urllib,sys,hmac,mimetypes,base64,array,uuid,json
+
+def post_multipart(host, selector, fields, files):
+	content_type, body = encode_multipart_formdata(fields, files)
+	h = httplib.HTTP(host)
+	h.putrequest('POST', selector)
+	h.putheader('content-type', content_type)
+	h.putheader('content-length', str(len(body)))
+	h.endheaders()
+	h.send(body)
+	errcode, errmsg, headers = h.getreply()
+	return errcode, errmsg, h.file.read()
+
+def encode_multipart_formdata(fields, files):
+	BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+	CRLF = '\r\n'
+	L = []
+	for key, value in fields.items():
+		L.append('--' + BOUNDARY)
+		L.append('Content-Disposition: form-data; name="%s"' % key)
+		L.append('')
+		L.append(value)
+	body = CRLF.join(L)
+	body = body + CRLF
+	for key, value in files.items():
+		body = body + '--' + BOUNDARY + CRLF
+		body = body + 'Content-Type: %s' % get_content_type(value['filename']) + CRLF
+		body = body + 'Content-Disposition: form-data; name="%s"; filename="%s"' % (key, value['filename']) + CRLF
+		body = body + CRLF
+		body = body.encode('utf-8') + value['content'] + CRLF
+	body = body + '--' + BOUNDARY + '--' + CRLF + CRLF
+	content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+	return content_type, body
+
+def get_content_type(filename):
+	return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
 
 class rest:
 	api_key = None
@@ -37,33 +73,57 @@ class rest:
 		return string.join(map(lambda v: "%02x" % ord(v), hash.digest()), "")
 
 	def set_type(self, mtype):
-		self.mtype = mtype
+		self.mtype = mtype.lower()
 
 	def set_image(self, image):
 		self.imgfile = image
 
+	def send(self,to='',sender='',text='',subject=''):
+		if to == '':
+			return 'No phone number input'
+		if text == '':
+			return 'No message input'
 
-	def send(self,to,sender,text):
-		#to = "01048597580,01048597580"
-		#to = "0100000000"
-		#sender = "01048597580"
-		#text = "Hello CoolSMS"
-		salt = "1234"
+		if type(to) == list:
+			to = ','.join(to)
+
+		salt = str(uuid.uuid1())
+		print 'salt : ' + salt
 		data = salt + self.user_id
+		print 'api secret : ' + self.api_secret
+		print 'data : ' + data
 		signature = hmac.new(self.api_secret, data, md5)
 
-		conn = httplib.HTTPConnection("apitest.coolsms.co.kr", 2000)
-		params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'type':self.mtype, 'to':to, 'from':sender, 'text':text})
+		host = "apitest.coolsms.co.kr:2000"
+		selector = "/1/send"
+		fields = {'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'type':self.mtype, 'to':to, 'text':text}
+		if sender:
+			fields['from'] = sender
+		if subject:
+			fields['subject'] = subject
 		if self.mtype == 'mms':
-			headers = {"Content-type": "multipart/form-data", "Accept": "text/plain"}
+			with open(self.imgfile, 'rb') as content_file:
+				content = content_file.read()
+			files = {'image':{'filename':self.imgfile,'content':content}}
 		else:
-			headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-			conn.request("POST", "/1/send", params, headers)
-		response = conn.getresponse()
-		print response.status, response.reason
-		data = response.read()
-		print data
-		conn.close()
+			files = {}
+
+		status, reason, response = post_multipart(host, selector, fields, files)
+		return json.loads(response)
+		#return response
+
+		#conn = httplib.HTTPConnection("apitest.coolsms.co.kr", 2000)
+		#params = urllib.urlencode({'api_key':self.api_key, 'salt':salt, 'signature':signature.hexdigest(), 'type':self.mtype, 'to':to, 'from':sender, 'text':text})
+		#if self.mtype == 'mms':
+		#	headers = {"Content-type": "multipart/form-data", "Accept": "text/plain"}
+		#else:
+		#	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		#	conn.request("POST", "/1/send", params, headers)
+		#response = conn.getresponse()
+		#print response.status, response.reason
+		#data = response.read()
+		#print data
+		#conn.close()
 
 	def status(self, mid = None, gid = None):
 		if mid == None and gid == None:
@@ -84,6 +144,7 @@ class rest:
 		data = response.read()
 		print data
 		conn.close()
+		return json.loads(data)
 
 	def balance(self):
 		salt = "1234"
