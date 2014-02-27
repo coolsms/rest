7226a3d81c4e91b0bbb9dff5be3d84d7f07d2738
hosysy <hosy@nurigo.net>
2014-02-27 15:10:58 +0900

    1.1fix

--- java/SetBase.java
+++ java/SetBase.java
@@ -1,7 +1,7 @@
 
 public class SetBase {
-	private String api_key = "NCS52A57F48C3D32";
-	private String api_secret = "5AC44E03CE8E7212D9D1AD9091FA9966";
+	private String api_key = "NCS521234C3D32";
+	private String api_secret = "5AC44123458E7212D9D1AD9091FA9966";
 	
 	public String getApiKey() {
 		return api_key;
