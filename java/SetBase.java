319ac3db19970b2b8b00d6e044fbe624006e4867
hosysy <hosy@nurigo.net>
2014-04-17 17:17:24 +0900

    받는사람 번호 변경

--- java/SetBase.java
+++ java/SetBase.java
@@ -1,7 +1,7 @@
 
 public class SetBase {
-	private String api_key = "NCS52A57F48C3D32";
-	private String api_secret = "5AC44E03CE8E7212D9D1AD9091FA9966";
+	private String api_key = "NCS52A57F48C2D42";
+	private String api_secret = "5AC44E03CE1E7312D9D1AD9091FA9966";
 	private String base_url="https://api.coolsms.co.kr/1/";
 	
 	public String getApiKey() {
